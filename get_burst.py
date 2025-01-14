54ab6f7c8b80e37e88a67d8d45c77dd237b72355
Holand13 <ha5933@mci4me.at>
2025-01-14 09:26:33 +0100

    verbesserung

--- get_burst.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import scipy.signal as signal
-import matplotlib.pyplot as plt
-
-from Lab3Functions import import_data
-weights, mvc, fatigue = import_data('\t')
-
-
